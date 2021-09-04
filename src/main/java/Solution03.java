8320ca0ebd6a2f362bbb64f39a9c12fe937d5ca6
Alexander <azoomer2@gmail.com>
2021-09-04 10:12:37 -0400

    added formatting and copyright

--- src/main/java/Solution03.java
+++ src/main/java/Solution03.java
@@ -1,13 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Alexander Zommer
+ */
 import java.util.Scanner;
 
 public class Solution03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the quote?");
+        System.out.print("What is the quote? ");
         String quote = scan.nextLine();
-        System.out.println("Who said it?");
+        System.out.print("Who said it? ");
         String author = scan.nextLine();
-        System.out.println(author + "says, \"" + quote + ".\"");
+        System.out.println(author + " says, \"" + quote + ".\"");
 
     }
 
