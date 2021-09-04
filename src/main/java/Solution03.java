5553413da42ef4cf19b2b77c8f3ed961ee3db9dd
Alexander <azoomer2@gmail.com>
2021-09-04 10:05:16 -0400

    Initial Commit

---
+++ src/main/java/Solution03.java
@@ -0,0 +1,14 @@
+public class Solution03 {
+
+}
+
+/*
+    Psuedo Code
+
+    System.in user for 'quote'
+    System.in user for 'author'
+    System.out 'author' + says + 'quote' using /"
+
+
+
+ */
